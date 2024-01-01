1cdeaa486805b30481637f0456dba252ed2e6b1c
khaledghazi <khaled.ghazi@informatik.hu-berlin.de>
2024-01-01 15:58:55 +0100

    edit2

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 import streamlit as st
 import pandas as pd
-import requests
-from io import BytesIO
 
-st.title('Large File Processor')
+st.title('CSV File Viewer')
 
-# User inputs the download link
-file_url = st.text_input("Enter the URL of the CSV file")
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-if file_url:
-    # Downloading the file
-    st.write("Downloading the file...")
-    response = requests.get(file_url)
-    response.raise_for_status()
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
 
-    # Assume the file is a CSV
-    data = pd.read_csv(BytesIO(response.content))
-    st.write(data)
+    st.write("Data from the CSV file:")
+    st.dataframe(data)
 
     # Example of a simple filter
     if st.checkbox('Show only selected columns'):
         all_columns = data.columns.tolist()
         selected_columns = st.multiselect("Select columns", all_columns, all_columns[0])
         new_df = data[selected_columns]
+        st.dataframe(new_df)
+
+    # Example of a simple chart
+    if st.button('Show Chart'):
+        st.line_chart(data)
